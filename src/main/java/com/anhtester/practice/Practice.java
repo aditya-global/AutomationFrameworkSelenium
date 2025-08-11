44346bbea268656e1b822ade2b8c29b11e982405
aditya-global <adityaguptaofficial2015@gmail.com>
2025-08-11 09:12:59 +0000

    test

--- src/main/java/com/anhtester/practice/Practice.java
+++ src/main/java/com/anhtester/practice/Practice.java
@@ -4,5 +4,6 @@ public class Practice {
     public static void main(String[] args) {
         //some practice code here
         //some comment to indicate practice code
+        //some more practice code
     }
 }
